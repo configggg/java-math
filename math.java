1644fc894e44c03a3d8deb10429c8c88225b526a
starts with j <123692783+configggg@users.noreply.github.com>
2023-08-06 00:54:16 +0700

    Add files via upload

---
+++ math.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class math {
+    public static void main (String[] args){
+        System.out.println(Math.max(12,22));
+        System.out.println(Math.min(12,22));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.abs(-9));
+        System.out.println(Math.round((3.72145)));
+        System.out.println(Math.ceil(3.5678));
+        System.out.println(Math.floor(3.5678));
+        System.out.println(Math.random());
+    }
+}
